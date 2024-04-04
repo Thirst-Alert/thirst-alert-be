c562a379923ac76dd6517fc2c5f008ce4bd7ec55
Alsje <alsje@mernok.co.za>
2024-04-04 17:01:57 +0200

    feat: 🎸 new measurement get route

--- src/test/integration/measurement/get.test.js
+++ src/test/integration/measurement/get.test.js
@@ -8,90 +8,27 @@ afterEach(async () => {
 describe('GET /measurement/:sensorId', () => {
 	describe('Authentication', () => {
 		it('should return 401 if client is not authenticated', async function () {
-			const res = await request(global.app).get('/measurement/65ecd6fbc66268658ae1fd09')
+			const res = await request(global.app).get('/measurement/65ecd6fbc66268658ae1fd09?view=week')
 			expect(res.status).toBe(401)
 		})
 	})
 
 	describe('Schema validation', () => {
-		it('should validate sensorId correctly', async function() {
+		it('should validate sensorId and view correctly', async function() {
 			const user = await db.createDummyUser()
-			const res = await authenticatedAgent(user).get('/measurement/123')
+			const res = await authenticatedAgent(user).get('/measurement/123?view=week')
 			expect(res.status).toBe(422)
 			expect(res.body.result[0].message).toBe('must match pattern "^[0-9a-fA-F]{24}$"')
-		})
-
-		it('should validate limit correctly', async function() {
-			const user = await db.createDummyUser()
-			const res1 = await authenticatedAgent(user).get('/measurement/65ecd6fbc66268658ae1fd09?limit=0')
+			const res1 = await authenticatedAgent(user).get('/measurement/65ecd6fbc66268658ae1fd09?view=something')
 			expect(res1.status).toBe(422)
-			expect(res1.body.result[0].message).toBe('must be >= 48')
-			const res2 = await authenticatedAgent(user).get('/measurement/65ecd6fbc66268658ae1fd09?limit=1537')
-			expect(res2.status).toBe(422)
-			expect(res2.body.result[0].message).toBe('must be <= 1536')
-			const res3 = await authenticatedAgent(user).get('/measurement/65ecd6fbc66268658ae1fd09?limit=test')
-			expect(res3.status).toBe(422)
-			expect(res3.body.result[0].message).toBe('must be integer')
-		})
-
-		it('should validate offset correctly', async function() {
-			const user = await db.createDummyUser()
-			const res1 = await authenticatedAgent(user).get('/measurement/65ecd6fbc66268658ae1fd09?offset=-1')
-			expect(res1.status).toBe(422)
-			expect(res1.body.result[0].message).toBe('must be >= 0')
-			const res2 = await authenticatedAgent(user).get('/measurement/65ecd6fbc66268658ae1fd09?offset=test')
-			expect(res2.status).toBe(422)
-			expect(res2.body.result[0].message).toBe('must be integer')
-		})
-
-		it('should validate sort correctly', async function() {
-			const user = await db.createDummyUser()
-			const res1 = await authenticatedAgent(user).get('/measurement/65ecd6fbc66268658ae1fd09?sort=0')
-			expect(res1.status).toBe(422)
-			expect(res1.body.result[0].message).toBe('must be equal to one of the allowed values')
-			const res2 = await authenticatedAgent(user).get('/measurement/65ecd6fbc66268658ae1fd09?sort=test')
-			expect(res2.status).toBe(422)
-			expect(res2.body.result[0].message).toBe('must be integer')
-		})
-
-		it('should validate startDate correctly', async function() {
-			const user = await db.createDummyUser()
-			const res = await authenticatedAgent(user).get('/measurement/65ecd6fbc66268658ae1fd09?startDate=notadate')
-			expect(res.status).toBe(422)
-			expect(res.body.result[0].message).toBe('must match format "date-time"')
+			expect(res1.body.result[0].message).toBe('must be equal to one of the allowed values')			
 		})
 	})
 
 	describe('Logic', () => {
-		it('should return measurements for a sensor', async function() {
-			const user = await db.createDummyUser()
-			const sensor = await db.createDummySensor(user._id)
-			await db.createDummyMeasurement(sensor._id, {
-				createdAt: new Date(Date.now() - 1000 * 60 * 60 * 0)
-			})
-			await db.createDummyMeasurement(sensor._id, {
-				createdAt: new Date(Date.now() - 1000 * 60 * 60 * 1)
-			})
-			await db.createDummyMeasurement(sensor._id, {
-				createdAt: new Date(Date.now() - 1000 * 60 * 60 * 2)
-			})
-			const res1 = await authenticatedAgent(user).get(`/measurement/${sensor._id}`)
-			expect(res1.status).toBe(200)
-			expect(res1.body.message).toBe('Measurements fetched successfully')
-			expect(res1.body.measurements.length).toBe(1)
-			const res2 = await authenticatedAgent(user).get(`/measurement/${sensor._id}?limit=48`)
-			expect(res2.body.measurements.length).toBe(1)
-			const res3 = await authenticatedAgent(user).get(`/measurement/${sensor._id}?offset=2`)
-			expect(res3.body.measurements.length).toBe(1)
-			const res4 = await authenticatedAgent(user).get(`/measurement/${sensor._id}?sort=-1`)
-			expect(res4.body.measurements.length).toBe(1)
-			const res5 = await authenticatedAgent(user).get(`/measurement/${sensor._id}?startDate=${new Date(Date.now() - 1000 * 60 * 60 * 1.5).toISOString()}`)
-			expect(res5.body.measurements.length).toBe(1)
-		})
-
 		it('should error if sensor does not exist', async function() {
 			const user = await db.createDummyUser()
-			const res = await authenticatedAgent(user).get('/measurement/65ecd6fbc66268658ae1fd09')
+			const res = await authenticatedAgent(user).get('/measurement/65ecd6fbc66268658ae1fd09?view=week')
 			expect(res.status).toBe(404)
 			expect(res.body.error.message).toBe('Sensor not found')
 		})
@@ -106,7 +43,7 @@ describe('GET /measurement/:sensorId', () => {
 				email: 'test2@email.com'
 			})
 			const sensor = await db.createDummySensor(user1._id)
-			const res = await authenticatedAgent(user2).get(`/measurement/${sensor._id}`)
+			const res = await authenticatedAgent(user2).get(`/measurement/${sensor._id}?view=week`)
 			expect(res.status).toBe(404)
 			expect(res.body.error.message).toBe('Sensor not found')
 		})
